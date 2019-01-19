8138a5b14b021ba5c5a61eb0a19e4ed8a28841c8
Claire Sinozich <claire.sinozich@gmail.com>
2019-01-19 11:38:20 -0700

    callbacks finished

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -21,27 +21,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function callbackFunction(x) {
+  console.log(x);
+}
 
-function getLength(arr, cb) {
+
   // getLength passes the length of the array into the callback.
+function getLength(arr, cb) {
+  return callbackFunction(arr.length);
 }
 
-function last(arr, cb) {
+getLength(items, callbackFunction);
+
+
   // last passes the last item of the array into the callback.
+function last(arr, cb) {
+  let lastItem = arr[arr.length-1]
+  return callbackFunction(lastItem)
 }
 
-function sumNums(x, y, cb) {
+last(items, callbackFunction);
+
+
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+function sumNums(x, y, cb) {
+  let summed = x+y;
+  return callbackFunction(summed);
 }
 
-function multiplyNums(x, y, cb) {
+sumNums(3, 7, callbackFunction);
+
+
   // multiplyNums multiplies two numbers and passes the result to the callback.
+function multiplyNums(x, y, cb) {
+  let multiplied = x*y;
+  return callbackFunction(multiplied);
 }
 
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+multiplyNums(4, 2, callbackFunction);
+
+
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
+function contains(str, arr, cb) {
+  let containedItem;
+  if (arr.includes(str)) {
+    containedItem = true;
+  }
+  else {
+    containedItem = false;
+  }
+  return callbackFunction(containedItem)
 }
+
+contains('Road', items, callbackFunction);
+contains('Pencil', items, callbackFunction);
 
 /* STRETCH PROBLEM */
 
